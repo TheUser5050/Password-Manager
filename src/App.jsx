1737399a0088e89452703d1d59a639b779147e6e
TheUser5050 <anwith5131@gmail.com>
2024-07-03 22:22:35 +0530

    NavBar & Some search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <NavBar />
+      <div className="flex items-center rounded-full justify-center my-4 w-screen">
+        <span className="material-symbols-outlined text-3xl bg-gray-700 rounded-l-full px-2 outline outline-gray-300 text-gray-300">
+          search
+        </span>
+        <input
+          type="search"
+          name="search"
+          className="bg-gray-700 outline text-xl h-10 outline-gray-300 px-2"
+          placeholder="Search"
+        />
+        <span className="material-symbols-outlined text-3xl bg-gray-700 rounded-r-full px-2 outline outline-gray-300">
+          close
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <div className="shadow-gray-950 shadow py-1 px-3">
+      <div className="text-3xl">
+        <p className="inline">Pass</p>
+        <p className="inline text-orange-600">Manager</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
